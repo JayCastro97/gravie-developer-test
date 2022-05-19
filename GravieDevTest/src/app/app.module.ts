a84527dc4f0d1aefd9166c0ab598698775ab8a9d
Jay Castro <33559928+JayCastro97@users.noreply.github.com>
2022-05-19 12:57:11 -0500

    setup Angular project and add landing page

---
+++ GravieDevTest/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FeaturedBannerComponent } from './featured-banner/featured-banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FeaturedBannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ GravieDevTest/src/app/featured-banner/featured-banner.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FeaturedBannerComponent } from './featured-banner.component';
+
+describe('FeaturedBannerComponent', () => {
+  let component: FeaturedBannerComponent;
+  let fixture: ComponentFixture<FeaturedBannerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FeaturedBannerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FeaturedBannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
